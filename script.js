0448ce3f843652b8da74a0c43c90f006b59056bb
orviepo <jhonjassergarcia@gmail.com>
2024-03-22 15:08:50 +0800

    Using new image for map

--- script.js
+++ script.js
@@ -1,50 +1,63 @@
-const getPos = document.getElementsByClassName('getPos')[0]
+const locate = document.getElementsByClassName('locate')[0];
 
-let positions = []
+let positions = [];
 
-clear = () => positions = []
+clear = () => positions = [];
 
 push = e => {
-  const x = Math.floor(e.clientX - getPos.getBoundingClientRect().x)
-  const y = Math.floor(e.clientY - getPos.getBoundingClientRect().y)
-  positions.push(x, y)
-  console.log(`Position stored: ${x}, ${y}`)
+  const x = Math.floor(e.clientX - locate.getBoundingClientRect().x);
+  const y = Math.floor(e.clientY - locate.getBoundingClientRect().y);
+  positions.push(x, y);
+  console.log(`Position stored: ${x}, ${y}`);
 }
 
 pop = () => {
-  if (positions.length === 0) return
-  const y = positions.pop()
-  const x = positions.pop()
-  console.log(`Removed: ${x}, ${y}`)
+  if (positions.length === 0) return;
+  const y = positions.pop();
+  const x = positions.pop();
+  console.log(`Removed: ${x}, ${y}`);
 }
 
 getPoly = () => {
-  navigator.clipboard.writeText(positions.toString())
-  console.log('Copied coords')
+  navigator.clipboard.writeText(positions.toString());
+  console.log('Copied coords');
 }
 
 ctrlCopy = e => {
   if (e.ctrlKey && e.key === 'c')
-    getPoly()
+    getPoly();
 }
 
 // Uncomment to turn on
-//getPos.onmousedown = push
+// locate.onmousedown = push;
 
 
-document.addEventListener('keydown', ctrlCopy)
+document.addEventListener('keydown', ctrlCopy);
 
 // adjust image map
 
 window.onload = () => {
-  const ogW = 396
-  const scale = getPos.getBoundingClientRect().width
-  const maps = document.getElementsByTagName('area')
+  const maps = document.getElementsByTagName('area');
+
   for (let map of maps) {
-    const coords = map.coords.split(',')
+    const coords = map.coords.split(',');
+
     for (let i in coords) {
-      coords[i] = Math.round(coords[i] * (scale / ogW))
+      const w = locate.getBoundingClientRect().width;
+      const h = locate.getBoundingClientRect().height;
+      const wog = 2135 * (w / h);
+      const hog = 2135;
+
+      if (i % 2 === 0) {
+        coords[i] = Math.round((coords[i]) * (w / wog));
+      } else {
+        coords[i] = Math.round((coords[i]) * (h / hog));
+      }
+
     }
-    map.coords = coords.join(',')
+
+    map.coords = coords.join(',');
   }
 }
+
+window.onresize = window.onload
